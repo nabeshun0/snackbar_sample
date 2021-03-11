3194d56c034d0693ab8b197f5b659bb6acd566ea
snt-nbzw <sntnbzw@gmail.com>
2021-03-11 11:49:11 +0900

    Add: implement snackbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,46 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  // runApp(MyApp());
+  runApp(MaterialApp(
+    home: SnackBarPage(),
+  ));
+}
+
+class SnackBarPage extends StatefulWidget {
+  @override
+  _SnackBarPageState createState() => _SnackBarPageState();
+}
+
+class _SnackBarPageState extends State<SnackBarPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('SnackBar Demo'),
+      ),
+      body: SnackBarBody(),
+    );
+  }
+}
+
+class SnackBarBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {
+          final snackBar = SnackBar(
+            content: Text('Yay! A SnackBar!'),
+            action: SnackBarAction(
+              label: 'Undo',
+              onPressed: () {},
+            ),
+          );
+
+          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+        },
+        child: Text('Show SnackBar'),
+      ),
+    );
+  }
 }
